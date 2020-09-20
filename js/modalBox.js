67367430992c50a7023fc077f4c99dca1e636df6
jacmill <jakub3020.jmy@gmail.com>
2020-09-20 22:55:17 +0200

    Positioned modal box a styled it

--- js/modalBox.js
+++ js/modalBox.js
@@ -1,12 +1,14 @@
 document.getElementById("createTask").addEventListener("click", openModal);
-document.getElementById("closeModalTaskBox").addEventListener("click", closeModal);
+document
+  .getElementById("closeModalTaskBox")
+  .addEventListener("click", closeModal);
 
 function openModal() {
-	document.getElementById("modalBox").style.display = "block";
-	document.getElementById("container").style.filter = "brightness(50%)";
+  document.getElementById("modalBox").style.display = "block";
+  document.getElementById("container").style.filter = "brightness(50%)";
 }
-function closeModal(){
-	document.getElementById("modalBox").style.display = "none";
-	document.getElementsById("container").style.filter = "brightness(100%)";
-    console.log("also works");
+function closeModal() {
+  document.getElementById("modalBox").style.display = "none";
+  document.getElementsById("container").style.filter = "brightness(100%)";
+  console.log("also works");
 }
