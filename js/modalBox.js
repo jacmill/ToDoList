2b7503a4be497fe530a794de8816bb91cd7fa59a
jacmill <jakub3020.jmy@gmail.com>
2020-09-30 00:18:33 +0200

    Made things more readable, added button for task, cleared navbar

--- js/modalBox.js
+++ js/modalBox.js
@@ -1,14 +1,11 @@
 document.getElementById("createTask").addEventListener("click", openModal);
-document
-  .getElementById("closeModalTaskBox")
-  .addEventListener("click", closeModal);
+document.getElementById("closeModalTaskBox").addEventListener("click", closeModal); 
 
 function openModal() {
   document.getElementById("modalBox").style.display = "block";
-  document.getElementById("container").style.filter = "brightness(50%)";
+  document.getElementById("main").style.filter = "brightness(50%)";
 }
 function closeModal() {
   document.getElementById("modalBox").style.display = "none";
-  document.getElementsById("container").style.filter = "brightness(100%)";
-  console.log("also works");
+  document.getElementById("main").style.filter = "none";
 }
