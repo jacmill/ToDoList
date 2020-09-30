4c9dfb15668549fe19824b17ad8ab02d7bb066a8
jacmill <jakub3020.jmy@gmail.com>
2020-10-01 00:11:13 +0200

    Added passing form values through javascript

---
+++ js/form.js
@@ -0,0 +1,8 @@
+
+function process(){
+    var title = document.getElementById("TaskName").value;
+    var desc = document.getElementById("Description").value;
+
+    console.log(title);
+    console.log(desc);
+}
