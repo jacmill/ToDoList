b37e0a68cf86e151710ef53923405341a5a7012a
jacmill <jakub3020.jmy@gmail.com>
2020-10-01 23:42:49 +0200

    Added simple div adding but will have to fix it later :P

--- js/form.js
+++
@@ -1,8 +0,0 @@
-
-function process(){
-    var title = document.getElementById("TaskName").value;
-    var desc = document.getElementById("Description").value;
-
-    console.log(title);
-    console.log(desc);
-}
