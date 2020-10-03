984aa7b7fde1eb959b0e92f0ffca333eddd5916e
jacmill <jakub3020.jmy@gmail.com>
2020-10-03 22:54:17 +0200

    .

--- js/createTask.js
+++ js/createTask.js
@@ -1,37 +1,55 @@
 var taskTitle;
 var taskDesc;
-var todocont = document.getElementById("TodoContainer");
-
+var todoCont = document.getElementById("TodoContainer");
+var taskNodeList = document.getElementsByClassName("Task");
 document.getElementById("SubmitButton").addEventListener("click", createTask);
 
+function createTask() {
+  //set form values as variables
+  taskTitle = document.getElementById("TaskName").value;
+  taskDesc = document.getElementById("Description").value;
+  // Title of Task
+  var titleNode = document.createElement("h1");
+  titleNode.className = "Title";
+  // Description of Task
+  var descNode = document.createElement("p");
+  descNode.className = "Description";
+  // Close Button
+  var deleteButtonWrapper = document.createElement("span");
+  deleteButtonWrapper.className = "deleteButton";
+  var btnImg = document.createElement("img");
+  btnImg.className = "btnImg";
 
-function createTask(){
-    taskTitle = document.getElementById("TaskName").value;
-    taskDesc = document.getElementById("Description").value;
-    var titleNode = document.createElement("h1");
-    titleNode.className = "Title";
-    var descNode = document.createElement("p");
-    descNode.className = "Description";
-    descNode.appendChild(document.createTextNode(taskDesc));
-    titleNode.appendChild(document.createTextNode(taskTitle));
+  btnImg.setAttribute("src", "../images/trashcan.svg");
+  deleteButtonWrapper.appendChild(btnImg);
 
-    var task = document.createElement("div");
-    task.className = "Task";
-    task.appendChild(titleNode);
-    task.appendChild(descNode);
-    /*
-    li.appendChild(title);
-    li.appendChild(desc);
-    */
-    if (taskTitle =="" || taskDesc == ""){
-        alert("You must write something");
+  descNode.appendChild(document.createTextNode(taskDesc));
+  titleNode.appendChild(document.createTextNode(taskTitle));
+
+  var task = document.createElement("div");
+  task.className = "Task";
+  var taskCont = document.createElement("div");
+  taskCont.className = "TaskContainer";
+
+  task.appendChild(taskCont);
+  taskCont.appendChild(titleNode);
+  taskCont.appendChild(descNode);
+  task.appendChild(deleteButtonWrapper);
+
+  if (taskTitle == "" || taskDesc == "") {
+    alert("You must write something");
+  } else {
+    todoCont.appendChild(task);
+  }
+
+  document.getElementById("modalBox").style.visibility = "hidden";
+  document.getElementById("main").style.filter = "none";
+  document.getElementById("TaskName").value = "";
+  document.getElementById("Description").value = "";
+}
+var close = document.getElementsByClassName("deleteButton");
+for(var i = 0; i < close.length; i++){
+    close[i].onclick = function(){
+        todoCont.removeChild(taskNodeList[i]);
     }
-    else{
-        todocont.appendChild(task);
-    }
-
-    document.getElementById("modalBox").style.visibility = "hidden";
-    document.getElementById("main").style.filter = "none";
-    document.getElementById("TaskName").value = "";
-    document.getElementById("Description").value = "";
 }
