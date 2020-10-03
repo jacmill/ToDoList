f76fc56cc1be4b72d4d101f85d4c9bd95c175d7f
jacmill <jakub3020.jmy@gmail.com>
2020-10-03 20:37:01 +0200

    Added proper task creating

--- js/createTask.js
+++ js/createTask.js
@@ -1,22 +1,37 @@
 var taskTitle;
 var taskDesc;
-document.getElementById("SubmitButton").addEventListener("click", getFormValues(), createTask());
+var todocont = document.getElementById("TodoContainer");
 
-function getFormValues(){
-    taskTitle = document.getElementById("TaskName").value;
-    taskDesc = document.getElementById("Description").value;
-}
+document.getElementById("SubmitButton").addEventListener("click", createTask);
+
 
 function createTask(){
-    var task = document.createElement("div");
-    task.setAttribute("class", "TaskBlock");
-    task.textContent = "NEW";
-    document.getElementById("TodoContainer").appendChild(task)
-    console.log("helo");
-}
+    taskTitle = document.getElementById("TaskName").value;
+    taskDesc = document.getElementById("Description").value;
+    var titleNode = document.createElement("h1");
+    titleNode.className = "Title";
+    var descNode = document.createElement("p");
+    descNode.className = "Description";
+    descNode.appendChild(document.createTextNode(taskDesc));
+    titleNode.appendChild(document.createTextNode(taskTitle));
 
-window.onbeforeunload = function(){
-    while (document.getElementById("TodoContainer").lastElementChild) {
-        document.getElementById("TodoContainer").removeChild(document.getElementById("TodoContainer").lastElementChild);
-      }
+    var task = document.createElement("div");
+    task.className = "Task";
+    task.appendChild(titleNode);
+    task.appendChild(descNode);
+    /*
+    li.appendChild(title);
+    li.appendChild(desc);
+    */
+    if (taskTitle =="" || taskDesc == ""){
+        alert("You must write something");
+    }
+    else{
+        todocont.appendChild(task);
+    }
+
+    document.getElementById("modalBox").style.visibility = "hidden";
+    document.getElementById("main").style.filter = "none";
+    document.getElementById("TaskName").value = "";
+    document.getElementById("Description").value = "";
 }
