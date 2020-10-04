2ffec880762bc37375eece44a070010c098981ca
jacmill <jakub3020.jmy@gmail.com>
2020-10-04 22:21:32 +0200

    Added forwarding task to another section

--- README.md
+++ README.md
@@ -3,9 +3,9 @@ This is my trello/todolist kind of thing. Work in progress.
 ## Roadmap 
 - [x] Creating Tasks
 - [x] Getting user input for taks title and description
-- [ ] Deleting Tasks
+- [x] Deleting Tasks
 - [ ] Editing Task
-- [ ] Draging to different levels of completion
+- [x] Draging to different levels of completion
 - [ ] Dark mode
 - [ ] Mobile Devices
 
--- js/Task.js
+++ js/Task.js
@@ -2,7 +2,7 @@ const todoInput = document.querySelectorAll(".todoInput");
 const todoList = document.querySelectorAll(".taskList");
 document.getElementById("SubmitButton").addEventListener("click", createTask);
 
-for(var i = 0; i < todoList.length; i++){
+for (var i = 0; i < todoList.length; i++) {
   todoList[i].addEventListener("click", deleteCheck);
 }
 
@@ -22,7 +22,7 @@ function createTask() {
   const todoDescription = document.createElement("p");
   todoDescription.classList.add("todoDesc");
   todoDescription.innerText = todoInput[1].value;
-  todoTextCont.appendChild(todoDescription)
+  todoTextCont.appendChild(todoDescription);
   //Putting together text for task
   todoDiv.appendChild(todoTextCont);
   //Complete button
@@ -44,18 +44,25 @@ function createTask() {
   document.getElementById("main").style.filter = "none";
 }
 
-function deleteCheck(e){
+function deleteCheck(e) {
   const button = e.target;
 
   //Delete Task
-  if(button.classList[0] === "trash-btn"){
+  if (button.classList[0] === "trash-btn") {
     const parent = button.parentElement;
-    parent.remove();
+    parent.classList.add("shrink");
+    parent.addEventListener("transitionend", function () {
+      parent.remove();
+    });
   }
 
   //Move Task to another section
-  if(button.classList[0] == "complete-btn"){
+  if (button.classList[0] == "complete-btn") {
     const parent = button.parentElement;
-    
+    var todoListArr = Array.prototype.slice.call(todoList);
+    var index = todoListArr.indexOf(parent.parentElement);
+    if (index != 2) {
+      todoList[index + 1].appendChild(parent);
+    }
   }
 }
